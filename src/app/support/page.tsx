752e767b7d2a5e5264220971a568cc7ee5f2708c
fero <hi@evan.graphics>
2024-02-07 20:33:12 -0700

    feat (pyro-27): redirect /support

---
+++ src/app/support/page.tsx
@@ -0,0 +1,9 @@
+import { redirect } from "next/navigation";
+
+const Page = () => {
+  redirect(
+    "https://pyro-host.notion.site/Help-Center-95642ed08e7746b295515d30f976f494",
+  );
+};
+
+export default Page;
