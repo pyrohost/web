02b145d2526496c95ec7de07908e3990fcd0e253
fero <hi@evan.graphics>
2024-02-09 15:36:50 -0700

    fix (pyro-29): migrate coming soon to sectionlayout

--- src/app/coming-soon/page.tsx
+++ src/app/coming-soon/page.tsx
@@ -1,3 +1,6 @@
+import BrandText from "@/components/sections/_components/BrandText";
+import ExternalLinkButton from "@/components/sections/_components/ExternalLinkButton";
+import Section from "@/components/sections/_components/SectionLayout";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
@@ -13,54 +16,22 @@ const Page = () => {
   `;
   return (
     <main className="relative h-full w-full">
-      <section className="relative h-screen w-full px-16">
-        <div className="mx-auto flex w-full max-w-[1620px] flex-col">
-          <div className="z-10 mx-auto flex w-full max-w-[735px] flex-col pt-60">
-            <h1 className="mt-4 text-[109px] font-extrabold leading-[98%] tracking-[-0.35rem]">
-              <span
-                className="bg-clip-text text-transparent"
-                style={{
-                  backgroundImage:
-                    "radial-gradient(109.26% 109.26% at 49.83% 13.37%, #FF343C 0%, #F06F53 100%)",
-                }}
-              >
-                Coming Soon
-              </span>
-            </h1>
-            <p className="mt-10 text-[22px] font-normal leading-[155%] text-[#ffffff99]">
-              {content}
-            </p>
-            <div className="mt-10 flex flex-row items-center gap-4">
-              <a
-                href="https://forms.gle/dUXavHV5BGL3QAheA"
-                rel="noopener noreferrer"
-                target="_blank"
-                style={{
-                  backgroundImage:
-                    "radial-gradient(109.26% 109.26% at 49.83% 13.37%, #FF343C 0%, #F06F53 100%)",
-                }}
-                className="flex items-center justify-center gap-4 rounded-full py-3 pl-8 pr-6 text-sm font-bold"
-              >
-                <p>Join the waitlist</p>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth={1.5}
-                  stroke="currentColor"
-                  className="h-6 w-6"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M4.5 12h15m0 0l-6.75-6.75M19.5 12l-6.75 6.75"
-                  />
-                </svg>
-              </a>
-            </div>
+      <Section hero>
+        <div className="mt-24 md:mt-48">
+          <Section.Title hero>
+            <BrandText>Coming Soon</BrandText>
+          </Section.Title>
+          <Section.Description hero maxWidth={800}>
+            {content}
+          </Section.Description>
+          <div className="mt-8">
+            <ExternalLinkButton
+              href="https://forms.gle/4Z5K5vzZz4tJqJ9P7"
+              text="Join the waitlist"
+            />
           </div>
         </div>
-      </section>
+      </Section>
     </main>
   );
 };
