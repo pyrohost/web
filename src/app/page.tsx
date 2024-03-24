a16fda9ca7500fc8be22b1aa19606659977d2818
fero <hi@evan.graphics>
2024-03-24 14:31:35 -0700

    chore: replace experience section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,10 @@ import ExperienceSection from "@/components/sections/ExperienceSection";
 import HeroSection from "@/components/sections/HeroSection";
 import CTASection from "@/components/sections/CTASection";
 import PanelSection from "@/components/sections/PanelSection";
+import RenderIfVisible from "@/components/ui/RenderIfVisible";
+import Cobe from "@/components/Cobe";
+import Section from "@/components/sections/_components/SectionLayout";
+import ExternalLinkButton from "@/components/sections/_components/ExternalLinkButton";
 
 export const metadata: Metadata = {
   title: "pyro - A better way to host games",
@@ -17,7 +21,28 @@ export default function Home() {
         <HeroSection />
         <PricingSection />
         <ServersSection />
-        <ExperienceSection />
+        <Section>
+          <div className="z-20">
+            <Section.Title maxWidth={800}>Go global with Pyro</Section.Title>
+            <Section.Description maxWidth={800}>
+              Our servers are currently in Europe and we&apos;re rapidly
+              expanding to more locations. But no matter where you are, we
+              strive for the best playing experience for you and your players.
+            </Section.Description>
+            <div className="mx-auto mt-8 w-fit">
+              <ExternalLinkButton
+                href="https://pay.pyro.host/checkout/config/3"
+                text="Order a server now"
+              />
+            </div>
+          </div>
+          <div className="absolute z-10 h-[400px] w-[800px] bg-black blur-[100px]"></div>
+          <div className="absolute">
+            <RenderIfVisible>
+              <Cobe />
+            </RenderIfVisible>
+          </div>
+        </Section>
         <PanelSection />
         <CTASection />
       </main>
