640b0050a9c4619964e39399bf76720825d4fa97
fero <hi@evan.graphics>
2024-04-06 20:20:30 -0700

    chore: update company page with new mission

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -17,10 +17,10 @@ const Page = () => {
             height={643}
             className="absolute top-12 scale-[0.8] mix-blend-lighten contrast-[1.05] [--padding:128] [--size:300] [--total:calc((2*var(--padding))+var(--size))] [mask:radial-gradient(calc(664/var(--total)*100%)_calc(389/var(--total)*100%)_at_50%_calc((var(--padding)-4)/var(--total)*100%),white,rgba(255,255,255,0.85)_58%,rgba(255,255,255,0))]"
           />
-          <div className="z-10 mx-auto mt-[24rem] flex h-full w-full max-w-[810px] flex-col items-center pt-20">
+          <div className="z-10 mx-auto mt-[24rem] flex h-full w-full max-w-[1010px] flex-col items-center pt-20">
             <h1 className="text-[52px] font-extrabold leading-[98%] tracking-[-0.14rem]">
               {/* eslint-disable-next-line react/no-unescaped-entities */}
-              Pyro's mission is to build the <br></br>
+              Pyro's mission is to power{" "}
               <span
                 className="bg-clip-text text-transparent"
                 style={{
@@ -28,8 +28,9 @@ const Page = () => {
                     "radial-gradient(109.26% 109.26% at 49.83% 13.37%, #FF343C 0%, #F06F53 100%)",
                 }}
               >
-                best hosting platform in the world.
-              </span>
+                hosting infrastructure
+              </span>{" "}
+              for the gaming industry
             </h1>
             <p className="mt-10 text-[22px] font-normal leading-[155%] text-[#ffffff99]">
               {/* eslint-disable-next-line react/no-unescaped-entities */}
