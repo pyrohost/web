5d2ab2456a8cf41520f9edd05d12a5ea94218ed7
fero <hi@evan.graphics>
2024-04-26 21:48:39 -0700

    chore `Alerion`: open >> Rust-powered

--- src/app/(projects)/alerion/page.tsx
+++ src/app/(projects)/alerion/page.tsx
@@ -12,7 +12,7 @@ const Page = () => {
                 <div className='relative mx-auto flex h-full w-full max-w-[1620px] flex-col items-center px-6'>
                     <div className='z-10 mx-auto mt-32 flex h-full w-full flex-row items-start justify-between'>
                         <h1 className='w-[45%] text-[45px] font-extrabold leading-[1.2] tracking-[-0.14rem] drop-shadow-xl'>
-                            Alerion is Pyro&apos;s open, cross-platform Wings successor.
+                            Alerion is Pyro&apos;s Rust-powered, cross-platform Wings successor.
                         </h1>
                         <div className='flex w-1/4 flex-col gap-4'>
                             <p className='text-[20px] leading-[155%] text-[#ffffffbb]'>
