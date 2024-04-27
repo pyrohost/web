58b41ae3855660c1e2222376c05d4e49e097a73c
fero <hi@evan.graphics>
2024-04-26 21:47:27 -0700

    chore `Alerion`: rewrite >> successor

--- src/app/(projects)/alerion/page.tsx
+++ src/app/(projects)/alerion/page.tsx
@@ -12,18 +12,18 @@ const Page = () => {
                 <div className='relative mx-auto flex h-full w-full max-w-[1620px] flex-col items-center px-6'>
                     <div className='z-10 mx-auto mt-32 flex h-full w-full flex-row items-start justify-between'>
                         <h1 className='w-[45%] text-[45px] font-extrabold leading-[1.2] tracking-[-0.14rem] drop-shadow-xl'>
-                            Alerion is Pyro&apos;s open, cross-platform Wings rewrite.
+                            Alerion is Pyro&apos;s open, cross-platform Wings successor.
                         </h1>
                         <div className='flex w-1/4 flex-col gap-4'>
                             <p className='text-[20px] leading-[155%] text-[#ffffffbb]'>
-                                Manage and run Dockerized game servers with Alerion. This is the future of Wings.
+                                Manage and run Dockerized game servers with Alerion. Super fast, super reliable.
                             </p>
                             <a className='flex items-center justify-center gap-4 rounded-full bg-white py-3 pl-8 pr-6 text-sm font-bold text-black'>
                                 <p>Star on GitHub</p>
                             </a>
                         </div>
                     </div>
-                    <div className='relative mt-8 min-h-[750px] w-full scale-[1.08]'>
+                    <div className='relative mt-8 min-h-[750px] w-full scale-[1.1]'>
                         <Image
                             src='/img/alerionhero.png'
                             width={1172}
