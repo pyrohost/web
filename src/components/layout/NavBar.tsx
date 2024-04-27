b75b598f4242f16805dbf5dcde9b4a1859e1b9ee
fero <hi@evan.graphics>
2024-04-26 21:45:20 -0700

    fix `NavBar`: use next link

--- src/components/layout/NavBar.tsx
+++ src/components/layout/NavBar.tsx
@@ -287,19 +287,20 @@ const ListItem = React.forwardRef<React.ElementRef<'a'>, React.ComponentPropsWit
         return (
             <li>
                 <NavigationMenuLink asChild>
-                    <a
+                    <Link
                         ref={ref}
                         className={cn(
                             'block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:text-accent-foreground focus-visible:bg-accent',
                             className,
                         )}
+                        href=''
                         {...props}
                     >
                         <div className='text-sm font-bold leading-none'>{title}</div>
                         <p className='line-clamp-2 text-sm font-medium leading-snug text-muted-foreground'>
                             {children}
                         </p>
-                    </a>
+                    </Link>
                 </NavigationMenuLink>
             </li>
         );
