fccabfd43c2780a7006faf95012ab23a303c2370
fero <hi@evan.graphics>
2024-04-30 15:44:58 -0700

    fix(ExternalLinkButton): use new tailwind variable

--- src/components/sections/_components/ExternalLinkButton.tsx
+++ src/components/sections/_components/ExternalLinkButton.tsx
@@ -4,10 +4,7 @@ const ExternalLinkButton = ({ href, text, ...other }: { href: string; text: stri
     return (
         <a
             data-standard-pyro-component='ExternalLinkButton'
-            style={{
-                backgroundImage: 'radial-gradient(109.26% 109.26% at 49.83% 13.37%, #FF343C 0%, #F06F53 100%)',
-            }}
-            className='flex items-center justify-center gap-4 rounded-full py-3 pl-8 pr-6 text-sm font-bold'
+            className='bg flex items-center justify-center gap-4 rounded-full bg-brandGrad py-3 pl-8 pr-6 text-sm font-bold'
             href={href}
             {...other}
         >
