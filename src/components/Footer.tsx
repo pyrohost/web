2fccc3be010a48e667e5a4dd2134cff15dfe3dc7
fero <hi@evan.graphics>
2024-05-02 21:05:08 -0700

    fix(footer): adjust sizing)

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,7 +5,7 @@ const Footer = () => {
         <footer className='sticky bottom-0 flex flex-col justify-between gap-12 px-4 py-7 text-white md:flex-col md:gap-x-12 md:px-10 lg:flex-row'>
             <div className='flex flex-col items-start gap-4 md:w-full lg:w-fit'>
                 <LogoWhite />
-                <p className='text-sm font-bold text-[#999]'>© 2024 Pyro Host Inc. All rights reserved.</p>
+                <p className='text-xs text-[#999]'>© 2024 Pyro Host Inc. All rights reserved.</p>
             </div>
         </footer>
     );
