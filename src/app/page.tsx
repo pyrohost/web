2ce0efeb1a8c5db2b9a19622656d126c6a99bd9b
fero <hi@evan.graphics>
2024-05-05 01:40:45 -0700

    chore: separate temporarility

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -162,6 +162,8 @@ export default function Home() {
                         </div>
                     </div>
                 </div>
+            </section>
+            <section className='relative z-10 w-full overflow-clip rounded-3xl rounded-t-none bg-white'>
                 <div
                     className='container relative grid gap-12 pb-20 pt-24 lg:gap-20 lg:py-40'
                     style={
