f570307c5f76bd5b2dab2e06f3c070047928b454
Evan Song <theevansong@gmail.com>
2024-05-12 21:30:22 -0700

    chore(projects): spacing

--- src/app/(projects)/alerion/page.tsx
+++ src/app/(projects)/alerion/page.tsx
@@ -22,7 +22,7 @@ const Page = () => {
         <div className='flex flex-1 flex-col overflow-x-clip xl:px-1 xl:pb-1'>
             <div className='relative z-10 max-h-[800px] min-h-[calc(100vh-76px)] w-full overflow-clip rounded-3xl bg-brand text-white'>
                 <div className='relative mx-auto flex h-full w-full max-w-[1620px] flex-col items-center px-6'>
-                    <div className='z-10 mx-auto mt-32 flex h-full w-full flex-col items-start justify-between gap-8 lg:flex-row lg:gap-0'>
+                    <div className='z-10 mx-auto mt-12 flex h-full w-full flex-col items-start justify-between gap-8 lg:flex-row lg:gap-0'>
                         <h1 className='w-full text-[max(30px,min(5vw,42px))] font-extrabold leading-[1.2] tracking-[-0.14rem] drop-shadow-xl lg:w-1/2'>
                             Alerion is Pyro&apos;s Rust-powered, cross-platform Wings successor
                         </h1>
