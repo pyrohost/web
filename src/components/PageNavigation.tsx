13c8e98182b36f4fb305ae2ed7a879b1a406ddc3
Evan Song <theevansong@gmail.com>
2024-05-16 20:17:06 -0700

    chore: clean

--- src/components/PageNavigation.tsx
+++ src/components/PageNavigation.tsx
@@ -13,7 +13,7 @@ const PageNavigation = () => {
                         <LogoColored />
                     </Link>
 
-                    <NavigationMenu.List className='w-full hidden items-center md:flex'>
+                    <NavigationMenu.List className='hidden w-full items-center md:flex'>
                         <NavigationMenu.Item>
                             <Link
                                 className='flex h-fit w-fit items-center gap-2 px-4 py-4 text-sm font-extrabold'
@@ -209,7 +209,6 @@ const PageNavigation = () => {
                             </Link>
                         </NavigationMenu.Item>
                         <NavigationMenu.Item>
-                            {' '}
                             <Link
                                 className='flex h-fit w-fit items-center gap-2 px-4 py-4 text-sm font-extrabold'
                                 rel='noopener noreferrer'
