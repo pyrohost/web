a64cd4525f5fffdb15bda2cc99527fbfd7e62078
fero <hi@evan.graphics>
2024-05-17 21:46:50 -0700

    refactor: use PyroButton on landing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -230,7 +230,9 @@ export default function Home() {
                         Pyro is a game hosting platform that makes it easier to play your favorite games with friends.
                         Incredible reliability, a beautiful game panel, and more.
                     </p>
-                    <PyroButton href='/for/'>Start your game server</PyroButton>
+                    <PyroButton useArrow href='/for/'>
+                        Start your game server
+                    </PyroButton>
                 </div>
             </div>
             <section className='relative z-10 w-full overflow-clip rounded-3xl rounded-t-none bg-gradient-to-b from-[#000000] to-[#121212] '>
@@ -256,12 +258,9 @@ export default function Home() {
                             It’s like a super-app for your server.
                         </p>
 
-                        <Link
-                            href='https://pay.pyro.host'
-                            className='flex w-fit items-center justify-center gap-4 rounded-full border-2 border-white/10 px-6 py-3 text-sm font-bold text-white'
-                        >
-                            <p>Learn more</p>
-                        </Link>
+                        <PyroButton color={'black'} type={'outlineLight'} href='/pyrodactyl/'>
+                            Learn more
+                        </PyroButton>
                     </div>
                     <div className='relative flex w-full flex-col gap-5 xl:flex-row'>
                         <div className='relative z-[14] grid w-full grid-flow-row gap-3 xl:gap-5'>
@@ -337,19 +336,23 @@ export default function Home() {
                     about how we operate, we iterate in coordination with our community, and we&apos;re always open to
                     contributions.
                 </div>
-                <Link
+                <PyroButton
+                    leftChild={
+                        <svg height='16' viewBox='0 0 14 14' width='16'>
+                            <path
+                                d='M7 .175c-3.872 0-7 3.128-7 7 0 3.084 2.013 5.71 4.79 6.65.35.066.482-.153.482-.328v-1.181c-1.947.415-2.363-.941-2.363-.941-.328-.81-.787-1.028-.787-1.028-.634-.438.044-.416.044-.416.7.044 1.071.722 1.071.722.635 1.072 1.641.766 2.035.59.066-.459.24-.765.437-.94-1.553-.175-3.193-.787-3.193-3.456 0-.766.262-1.378.721-1.881-.065-.175-.306-.897.066-1.86 0 0 .59-.197 1.925.722a6.754 6.754 0 0 1 1.75-.24c.59 0 1.203.087 1.75.24 1.335-.897 1.925-.722 1.925-.722.372.963.131 1.685.066 1.86.46.48.722 1.115.722 1.88 0 2.691-1.641 3.282-3.194 3.457.24.219.481.634.481 1.29v1.926c0 .197.131.415.481.328C11.988 12.884 14 10.259 14 7.175c0-3.872-3.128-7-7-7z'
+                                fill='currentColor'
+                                fillRule='nonzero'
+                            ></path>
+                        </svg>
+                    }
+                    color={'black'}
+                    type={'outlineLight'}
+                    external
                     href='https://github.com/pyrohost'
-                    className='relative z-10 flex w-fit items-center justify-center gap-4 rounded-full border-2 border-white/20 bg-black px-6 py-3 text-sm font-bold'
                 >
-                    <svg height='16' viewBox='0 0 14 14' width='16'>
-                        <path
-                            d='M7 .175c-3.872 0-7 3.128-7 7 0 3.084 2.013 5.71 4.79 6.65.35.066.482-.153.482-.328v-1.181c-1.947.415-2.363-.941-2.363-.941-.328-.81-.787-1.028-.787-1.028-.634-.438.044-.416.044-.416.7.044 1.071.722 1.071.722.635 1.072 1.641.766 2.035.59.066-.459.24-.765.437-.94-1.553-.175-3.193-.787-3.193-3.456 0-.766.262-1.378.721-1.881-.065-.175-.306-.897.066-1.86 0 0 .59-.197 1.925.722a6.754 6.754 0 0 1 1.75-.24c.59 0 1.203.087 1.75.24 1.335-.897 1.925-.722 1.925-.722.372.963.131 1.685.066 1.86.46.48.722 1.115.722 1.88 0 2.691-1.641 3.282-3.194 3.457.24.219.481.634.481 1.29v1.926c0 .197.131.415.481.328C11.988 12.884 14 10.259 14 7.175c0-3.872-3.128-7-7-7z'
-                            fill='currentColor'
-                            fillRule='nonzero'
-                        ></path>
-                    </svg>
-                    <p>Star our work on GitHub</p>
-                </Link>
+                    Star our work on GitHub
+                </PyroButton>
                 <Image
                     src='/img/transparency-img2x.png'
                     alt=''
@@ -417,13 +420,15 @@ export default function Home() {
                             included.
                         </p>
 
-                        <Link
+                        <PyroButton
+                            useArrow
+                            color={'black'}
+                            type={'outlineLight'}
                             href='https://pay.pyro.host'
-                            className='flex w-fit items-center justify-center gap-4 rounded-full border-2 border-white/10 px-6 py-3 text-sm font-bold'
+                            external
                         >
-                            <p>Host your game server, risk-free</p>
-                            <ArrowRightIcon className='h-4 w-4' />
-                        </Link>
+                            Host your server, risk-free
+                        </PyroButton>
                     </div>
                     <div className='relative flex w-full flex-col gap-5 md:flex-row'>
                         <div className='relative z-[14] grid w-full grid-flow-row gap-3 xl:gap-5'>
@@ -538,7 +543,9 @@ export default function Home() {
                     Zero waiting. Zero hassle. Pyro&apos;s servers are globally available and ready to play on as soon
                     as you subscribe. Choose your game, and we&apos;ll take care of the rest.
                 </div>
-                <PyroButton href='/for/'>Start your game server</PyroButton>
+                <PyroButton useArrow href='/for/'>
+                    Start your game server
+                </PyroButton>
                 <div className='min-h-[200px]' />
                 <Image
                     src='/img/globenew.png'
@@ -594,18 +601,22 @@ export default function Home() {
                             scales, and incredible uptime, you&apos;ll never want to go back.
                         </p>
 
-                        <Link
+                        <PyroButton
+                            leftChild={
+                                <svg viewBox='0 0 16 16' className='h-4 w-4'>
+                                    <path
+                                        fill='currentColor'
+                                        d='M13.538 2.997A13.092 13.092 0 0 0 10.285 2a.07.07 0 0 0-.054.023c-.137.247-.297.57-.404.817a12.456 12.456 0 0 0-3.657 0 7.468 7.468 0 0 0-.411-.817C5.75 2.008 5.729 2 5.705 2a13.192 13.192 0 0 0-3.253.997c-.008 0-.015.008-.023.015C.357 6.064-.215 9.033.067 11.972c0 .015.008.03.023.038 1.371.99 2.69 1.59 3.993 1.987.022.007.045 0 .053-.015.305-.412.579-.847.815-1.305.015-.03 0-.06-.03-.067a9.446 9.446 0 0 1-1.25-.585c-.03-.015-.03-.06-.008-.083.084-.06.168-.127.252-.187a.048.048 0 0 1 .053-.008c2.621 1.178 5.448 1.178 8.039 0a.048.048 0 0 1 .053.008c.084.067.167.127.251.195.03.022.03.067-.007.082-.396.233-.816.42-1.25.585-.03.008-.038.045-.03.068.244.457.518.892.815 1.304.023.008.046.015.069.008a13.266 13.266 0 0 0 4-1.987.041.041 0 0 0 .023-.038c.335-3.396-.557-6.343-2.362-8.96-.008-.007-.016-.015-.031-.015Zm-8.19 7.183c-.785 0-1.44-.712-1.44-1.59 0-.876.64-1.589 1.44-1.589.807 0 1.447.72 1.44 1.59 0 .877-.64 1.59-1.44 1.59Zm5.31 0c-.785 0-1.44-.712-1.44-1.59 0-.876.64-1.589 1.44-1.589.808 0 1.448.72 1.44 1.59 0 .877-.632 1.59-1.44 1.59Z'
+                                    ></path>
+                                </svg>
+                            }
+                            color={'black'}
+                            type={'outlineLight'}
+                            external
                             href='https://pyro.host/discord'
-                            className='flex w-fit items-center justify-center gap-4 rounded-full border-2 border-white/10 px-6 py-3 text-sm font-bold'
                         >
-                            <svg viewBox='0 0 16 16' className='h-4 w-4'>
-                                <path
-                                    fill='currentColor'
-                                    d='M13.538 2.997A13.092 13.092 0 0 0 10.285 2a.07.07 0 0 0-.054.023c-.137.247-.297.57-.404.817a12.456 12.456 0 0 0-3.657 0 7.468 7.468 0 0 0-.411-.817C5.75 2.008 5.729 2 5.705 2a13.192 13.192 0 0 0-3.253.997c-.008 0-.015.008-.023.015C.357 6.064-.215 9.033.067 11.972c0 .015.008.03.023.038 1.371.99 2.69 1.59 3.993 1.987.022.007.045 0 .053-.015.305-.412.579-.847.815-1.305.015-.03 0-.06-.03-.067a9.446 9.446 0 0 1-1.25-.585c-.03-.015-.03-.06-.008-.083.084-.06.168-.127.252-.187a.048.048 0 0 1 .053-.008c2.621 1.178 5.448 1.178 8.039 0a.048.048 0 0 1 .053.008c.084.067.167.127.251.195.03.022.03.067-.007.082-.396.233-.816.42-1.25.585-.03.008-.038.045-.03.068.244.457.518.892.815 1.304.023.008.046.015.069.008a13.266 13.266 0 0 0 4-1.987.041.041 0 0 0 .023-.038c.335-3.396-.557-6.343-2.362-8.96-.008-.007-.016-.015-.031-.015Zm-8.19 7.183c-.785 0-1.44-.712-1.44-1.59 0-.876.64-1.589 1.44-1.589.807 0 1.447.72 1.44 1.59 0 .877-.64 1.59-1.44 1.59Zm5.31 0c-.785 0-1.44-.712-1.44-1.59 0-.876.64-1.589 1.44-1.589.808 0 1.448.72 1.44 1.59 0 .877-.632 1.59-1.44 1.59Z'
-                                ></path>
-                            </svg>
-                            <p>Contact Us</p>
-                        </Link>
+                            Contact Us
+                        </PyroButton>
                     </div>
                     <div className='relative flex w-full flex-col gap-5 md:flex-row'>
                         <div className='relative z-[14] grid w-full grid-flow-row gap-3 xl:gap-5'>
