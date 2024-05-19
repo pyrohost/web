a6ddac6f042696ce4e1c93ff41d1d2b640a5ebd1
Evan Song <theevansong@gmail.com>
2024-05-18 23:31:15 -0700

    chore(a11y): text inside pyrobutton should not be selectable

--- src/components/ui/PyroButton.tsx
+++ src/components/ui/PyroButton.tsx
@@ -4,7 +4,7 @@ import type { VariantProps } from 'class-variance-authority';
 import { cva, cx } from 'class-variance-authority';
 import { twMerge } from 'tailwind-merge';
 
-const button = cva(['relative', 'flex', 'items-center', 'justify-center', 'rounded-full', 'font-bold', 'gap-4'], {
+const button = cva(['relative', 'flex', 'items-center', 'justify-center', 'rounded-full', 'font-bold', 'gap-4', 'select-none'], {
     variants: {
         color: {
             brand: ['bg-brand', 'text-white'],
