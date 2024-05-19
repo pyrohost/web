9485df39b9498bf98f5f7340c44ce76b112ba5e8
Evan Song <theevansong@gmail.com>
2024-05-19 14:11:23 -0700

    fix(a11y): ensure footer links are sufficiently described

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -22,7 +22,12 @@ const Footer = () => {
                 </div>
             </div>
             <div className='flex flex-row items-center justify-center gap-8'>
-                <a rel='noopener noreferrer' target='_blank' href='https://twitter.com/withPyro'>
+                <a
+                    aria-label='Follow Pyro on X (Formerly Twitter)'
+                    rel='noopener noreferrer'
+                    target='_blank'
+                    href='https://twitter.com/withPyro'
+                >
                     <svg height='16' strokeLinejoin='round' viewBox='0 0 16 16' width='16'>
                         <path
                             fillRule='evenodd'
@@ -33,7 +38,12 @@ const Footer = () => {
                     </svg>
                 </a>
 
-                <a rel='noopener noreferrer' target='_blank' href='https://github.com/pyrohost'>
+                <a
+                    aria-label='Follow Pyro on GitHub'
+                    rel='noopener noreferrer'
+                    target='_blank'
+                    href='https://github.com/pyrohost'
+                >
                     <svg height='16' viewBox='0 0 14 14' width='16'>
                         <path
                             d='M7 .175c-3.872 0-7 3.128-7 7 0 3.084 2.013 5.71 4.79 6.65.35.066.482-.153.482-.328v-1.181c-1.947.415-2.363-.941-2.363-.941-.328-.81-.787-1.028-.787-1.028-.634-.438.044-.416.044-.416.7.044 1.071.722 1.071.722.635 1.072 1.641.766 2.035.59.066-.459.24-.765.437-.94-1.553-.175-3.193-.787-3.193-3.456 0-.766.262-1.378.721-1.881-.065-.175-.306-.897.066-1.86 0 0 .59-.197 1.925.722a6.754 6.754 0 0 1 1.75-.24c.59 0 1.203.087 1.75.24 1.335-.897 1.925-.722 1.925-.722.372.963.131 1.685.066 1.86.46.48.722 1.115.722 1.88 0 2.691-1.641 3.282-3.194 3.457.24.219.481.634.481 1.29v1.926c0 .197.131.415.481.328C11.988 12.884 14 10.259 14 7.175c0-3.872-3.128-7-7-7z'
@@ -43,7 +53,12 @@ const Footer = () => {
                     </svg>
                 </a>
 
-                <a rel='noopener noreferrer' target='_blank' href='https://discord.gg/fxeRFRbhQh'>
+                <a
+                    aria-label='Join the Pyro Discord community'
+                    rel='noopener noreferrer'
+                    target='_blank'
+                    href='https://discord.gg/fxeRFRbhQh'
+                >
                     <svg viewBox='0 0 16 16' className='h-4 w-4'>
                         <path
                             fill='currentColor'
