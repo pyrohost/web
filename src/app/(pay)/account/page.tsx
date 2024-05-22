51c031f17a2081eae9a73c0341deff9d6d6e385c
fero <hi@evan.graphics>
2024-05-22 14:51:38 -0700

    fix: spacing

--- src/app/(pay)/account/page.tsx
+++ src/app/(pay)/account/page.tsx
@@ -34,8 +34,8 @@ const Page = async () => {
     }
 
     return (
-        <div className='container mx-auto flex flex-col gap-4'>
-            <h1 className='text-4xl font-bold'>Account</h1>
+        <div className='container mx-auto flex flex-col gap-4 lg:py-4'>
+            <h1 className='text-[max(24px,min(5vw,48px))] font-extrabold leading-[1.09] tracking-tighter'>Account</h1>
             <Suspense fallback={<DashboardSkeletonSection title={'Account Information'} />}>
                 <StripeInformation customerId={user.stripeCustomerId ?? ''} />
             </Suspense>
