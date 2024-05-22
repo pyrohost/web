40028adf7003d1782886ee6e06342f3a7bc0aee3
fero <hi@evan.graphics>
2024-05-22 13:55:38 -0700

    feat: merge stripe components

---
+++ src/components/stripe/StripePortalButton.tsx
@@ -0,0 +1,41 @@
+'use client';
+
+import { PencilIcon } from '../ui/Icons';
+import { createPortalSession } from '@/app/actions/stripe';
+import { useState } from 'react';
+
+const StripePortalButton = ({ customerID }: { customerID: string }) => {
+    const [formState, setFormState] = useState<string>('');
+
+    if (!customerID || customerID === '') {
+        return <p>No customer ID provided</p>;
+    }
+
+    const formAction = async (data: FormData): Promise<void> => {
+        data.append('customer', customerID);
+        try {
+            const { url } = await createPortalSession(data);
+
+            window.location.assign(url as string);
+        } catch (error) {
+            setFormState('An error occurred connecting to Stripe.');
+        }
+    };
+
+    return (
+        <form action={formAction}>
+            <button
+                className='flex items-center justify-center gap-2 rounded-full bg-[#ffffff33] px-3 py-1.5 text-sm font-bold text-white transition hover:bg-[#ffffff44]'
+                onClick={(e) => {
+                    e.stopPropagation();
+                }}
+                type='submit'
+            >
+                <PencilIcon /> Edit
+            </button>
+            {formState ? <p className='mt-2 text-sm font-bold text-red-500'>{formState}</p> : null}
+        </form>
+    );
+};
+
+export default StripePortalButton;
---
+++ src/components/stripe/StripeResumeSubscription.tsx
@@ -0,0 +1,46 @@
+'use client';
+
+import LoadingIcon from '../ui/LoadingIcon';
+import { resumeSubscription } from '@/app/actions/stripe';
+import { useRouter } from 'next/navigation';
+import { useState } from 'react';
+
+const StripeResumeSubscription = ({ subscriptionId }: { subscriptionId: string }) => {
+    const router = useRouter();
+    const [isLoading, setIsLoading] = useState<boolean>(false);
+
+    if (!subscriptionId || subscriptionId === '') {
+        return <p>No subscriptionId ID provided</p>;
+    }
+
+    const formAction = async (data: FormData): Promise<void> => {
+        if (!/^sub_\w+$/.test(subscriptionId)) {
+            throw new Error('Invalid subscription ID');
+        }
+
+        data.append('subscriptionId', subscriptionId);
+
+        await resumeSubscription(data);
+        return router.refresh();
+    };
+
+    return (
+        <form action={formAction}>
+            <button
+                onClick={() => setIsLoading(true)}
+                className='flex items-center justify-center gap-2 rounded-full bg-[#ffffff33] px-3 py-1.5 text-sm font-bold text-white'
+                type='submit'
+            >
+                {isLoading ? (
+                    <>
+                        <LoadingIcon /> Resuming...
+                    </>
+                ) : (
+                    <>Resume Subscription</>
+                )}
+            </button>
+        </form>
+    );
+};
+
+export default StripeResumeSubscription;
