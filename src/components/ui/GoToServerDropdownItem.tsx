8f46c7f8c732571f92887188c3394bd76ccde81c
fero <hi@evan.graphics>
2024-05-22 14:03:00 -0700

    feat: merge in actioned dropdownmenu

---
+++ src/components/ui/GoToServerDropdownItem.tsx
@@ -0,0 +1,23 @@
+'use client';
+
+import { DropdownMenuItem } from './DropdownMenu';
+import { getServerByExternalIdAction } from '@/actions/pterodactyl';
+import { useRouter } from 'next/navigation';
+
+const GoToServerDropdownItem = ({ subscriptionId }: { subscriptionId: string }) => {
+    const router = useRouter();
+
+    const goToServer = async (subscriptionId: string) => {
+        const server = await getServerByExternalIdAction(subscriptionId);
+
+        if (server) {
+            router.push(`https://panel.pyro.host/server/${server.identifier}`);
+        } else {
+            console.log(`No server found with external ID ${subscriptionId}.`);
+        }
+    };
+
+    return <DropdownMenuItem onSelect={() => goToServer(subscriptionId)}>Manage Server</DropdownMenuItem>;
+};
+
+export default GoToServerDropdownItem;
