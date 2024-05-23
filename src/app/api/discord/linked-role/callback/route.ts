061e643e7ab2b503dfaa3c1b211df9e52161f8f7
Checksum <checksum@pyro.host>
2024-05-23 13:51:39 -0500

    fix: cron to sync linked roles daily

--- src/app/api/discord/linked-role/callback/route.ts
+++ src/app/api/discord/linked-role/callback/route.ts
@@ -1,4 +1,3 @@
-import { cookies } from 'next/headers';
 import { redirect } from 'next/navigation';
 import { type NextRequest } from 'next/server';
 
@@ -7,50 +6,6 @@ import discord from '@/lib/discord';
 import prisma from '@/lib/prisma';
 import { stripe } from '@/lib/stripe';
 
-// /**
-//  * Route configured in the Discord developer console, the redirect Url to which
-//  * the user is sent after approving the bot for their Discord account. This
-//  * completes a few steps:
-//  * 1. Uses the code to acquire Discord OAuth2 tokens
-//  * 2. Uses the Discord Access Token to fetch the user profile
-//  * 3. Stores the OAuth2 Discord Tokens in Redis / Firestore
-//  * 4. Lets the user know it's all good and to go back to Discord
-//  */
-// app.get('/discord-oauth-callback', async (req, res) => {
-//     try {
-//       // 1. Uses the code and state to acquire Discord OAuth2 tokens
-//       const code = req.query['code'];
-//       const discordState = req.query['state'];
-
-//       // make sure the state parameter exists
-//       const { clientState } = req.signedCookies;
-//       if (clientState !== discordState) {
-//         console.error('State verification failed.');
-//         return res.sendStatus(403);
-//       }
-
-//       const tokens = await discord.getOAuthTokens(code);
-
-//       // 2. Uses the Discord Access Token to fetch the user profile
-//       const meData = await discord.getUserData(tokens);
-//       const userId = meData.user.id;
-//       await storage.storeDiscordTokens(userId, {
-//         access_token: tokens.access_token,
-//         refresh_token: tokens.refresh_token,
-//         expires_at: Date.now() + tokens.expires_in * 1000,
-//       });
-
-//       // 3. Update the users metadata, assuming future updates will be posted to the `/update-metadata` endpoint
-//       await updateMetadata(userId);
-
-//       res.send('You did it!  Now go back to Discord.');
-//     } catch (e) {
-//       console.error(e);
-//       res.sendStatus(500);
-//     }
-//   });
-
-// converted to nextjs:
 export async function GET(req: NextRequest, res: Response) {
     let session = await auth();
     if (!session) {
@@ -61,7 +16,7 @@ export async function GET(req: NextRequest, res: Response) {
     const code = searchParams.get('code');
 
     const tokens = await discord.getOAuthTokens(code as string);
-    const discordUser = await discord.getUserData(tokens);
+    const discordUser = await discord.getUserData(tokens.access_token);
 
     prisma.user.update({
         where: {
@@ -98,7 +53,10 @@ export async function GET(req: NextRequest, res: Response) {
         return redirect('/account?error=NoActiveSubscriptionsDiscordLinkedRole');
     }
 
-    await discord.pushMetadata(tokens, { services: subscriptions.data.length });
+    await discord.pushMetadata(tokens, {
+        services: subscriptions.data.length,
+        customer_since: dbUser.createdAt,
+    });
 
     return redirect('/account/?success=DiscordLinkedRole');
 }
---
+++ src/app/api/discord/linked-role/sync/route.ts
@@ -0,0 +1,60 @@
+import type { NextRequest } from 'next/server';
+
+import discord, { TokenInfo } from '@/lib/discord';
+import prisma from '@/lib/prisma';
+import { stripe } from '@/lib/stripe';
+
+export async function GET(request: NextRequest) {
+    const authHeader = request.headers.get('authorization');
+    if (authHeader !== `Bearer ${process.env.CRON_SECRET}`) {
+        return new Response('Unauthorized', {
+            status: 401,
+        });
+    }
+
+    const users = await prisma.user.findMany({
+        where: { discordLinkedRole: { NOT: undefined } },
+    });
+
+    for (const user of users) {
+        try {
+            const data = await prisma.discordLinkedRole.findUniqueOrThrow({
+                where: { userId: user.id },
+            });
+
+            let tokens: TokenInfo = {
+                access_token: data.accessToken,
+                refresh_token: data.refreshToken,
+                expires_at: data.expiresAt,
+            };
+
+            const accessToken = await discord.getAccessToken(tokens);
+            tokens.access_token = accessToken.access_token;
+            tokens.expires_at = accessToken.expires_at;
+
+            prisma.discordLinkedRole.update({
+                where: { userId: user.id },
+                data: {
+                    accessToken: accessToken.access_token,
+                    expiresAt: accessToken.expires_at,
+                },
+            });
+
+            const subscriptions = await stripe.subscriptions.list({
+                customer: user.stripeCustomerId!,
+                status: 'active',
+            });
+
+            await discord.pushMetadata(accessToken, {
+                services: subscriptions.data.length,
+                customer_since: user.createdAt,
+            });
+        } catch (error) {
+            console.error(`Error running cron job for user ${user.id}: ${error}`);
+        }
+    }
+
+    return new Response('Success', {
+        status: 200,
+    });
+}
