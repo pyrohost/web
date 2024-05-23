d1defd9924be81327d333a3421d2c47e19c42e1c
Evan Song <theevansong@gmail.com>
2024-05-23 15:50:55 -0700

    fix: allow closing account linking banner

--- src/app/(pay)/account/page.tsx
+++ src/app/(pay)/account/page.tsx
@@ -1,6 +1,9 @@
 import React, { Suspense } from 'react';
 
+import Link from 'next/link';
 import { redirect } from 'next/navigation';
+
+import { Cross1Icon } from '@radix-ui/react-icons';
 
 import AccountConnections from '@/components/AccountConnections';
 import DashboardSkeletonSection from '@/components/DashboardSkeletonSection';
@@ -34,17 +37,23 @@ const Page = async ({ searchParams }: { searchParams: { [key: string]: string | 
         <div className='container mx-auto flex flex-col gap-4 lg:py-4'>
             <h1 className='text-[max(24px,min(5vw,48px))] font-extrabold leading-[1.09] tracking-tighter'>Account</h1>
             {searchParams?.error === 'NoActiveSubscriptionsDiscordLinkedRole' && (
-                <div className='relative rounded-full bg-red-800/20 px-4 py-3 text-red-400' role='alert'>
-                    <strong className='font-bold'>Failed to link Discord account!</strong>
-                    <span className='block sm:inline'>
-                        {''} You need to purchase something before linking your Discord.
+                <div className='relative flex rounded-full bg-red-800/20 px-4 py-3 text-red-400' role='alert'>
+                    <strong className='font-bold'>Failed to link Discord account! </strong>
+                    <span className='ml-0.5 block sm:inline'>
+                        You need to purchase something before linking your Discord.
                     </span>
+                    <Link aria-label='Close alert' className='ml-auto' href='/account'>
+                        <Cross1Icon className=' h-6 w-6' />
+                    </Link>
                 </div>
             )}
             {searchParams?.success === 'DiscordLinkedRole' && (
-                <div className='relative rounded-full bg-brand px-4 py-3 text-white' role='alert'>
-                    <strong className='font-bold'>Your Discord account was linked!</strong>
-                    <span className='block sm:inline'>{''} Check your roles on the Pyro Discord server!</span>
+                <div className='relative flex rounded-full bg-red-800/20 px-4 py-3 text-red-400' role='alert'>
+                    <strong className='font-bold'>Your Discord account is now linked!</strong>
+                    <span className='ml-0.5 block sm:inline'>Check your roles on the Pyro Discord server!</span>
+                    <Link aria-label='Close alert' className='ml-auto' href='/account'>
+                        <Cross1Icon className=' h-6 w-6' />
+                    </Link>
                 </div>
             )}
             <Suspense fallback={<DashboardSkeletonSection title={'Account Information'} />}>
