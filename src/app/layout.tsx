8e5e262d984b285f741eb9d46e8377c7890b2578
fero <hi@evan.graphics>
2024-05-25 03:07:30 -0700

    chore: experimental X insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import { GoogleAnalytics } from '@next/third-parties/google';
 
 import type { Metadata, Viewport } from 'next';
 import { Plus_Jakarta_Sans } from 'next/font/google';
+import Script from 'next/script';
 
 import Footer from '@/components/Footer';
 import HeaderLoginButton from '@/components/HeaderLoginButton';
@@ -55,6 +56,12 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
                     <Footer />
                 </div>
             </body>
+            <Script id='x-insights'>
+                {`!function(e,t,n,s,u,a){e.twq||(s=e.twq=function(){s.exe?s.exe.apply(s,arguments):s.queue.push(arguments);
+},s.version='1.1',s.queue=[],u=t.createElement(n),u.async=!0,u.src='https://static.ads-twitter.com/uwt.js',
+a=t.getElementsByTagName(n)[0],a.parentNode.insertBefore(u,a))}(window,document,'script');
+twq('config','om9ft');`}
+            </Script>
             <GoogleAnalytics gaId='G-NWVJ0FNXG1' />
         </html>
     );
