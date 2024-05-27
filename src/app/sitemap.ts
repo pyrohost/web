d3ce3c8ee8217e4e09a0c24cd664e2da2224b93f
fero <hi@evan.graphics>
2024-05-27 16:47:58 -0700

    chore: dynamically generate sitemap for dynamic game pages

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,5 +1,18 @@
 import type { MetadataRoute } from "next";
 
+import { games } from "@/lib/static/static";
+
+function generateGameMap(games: { name: string; slug: string; images: { banner: string } }[]): MetadataRoute.Sitemap {
+	return games.map(game => {
+		return {
+			url: `https://pyro.host/for/${game.slug}`,
+			lastModified: new Date(),
+			changeFrequency: "always",
+			priority: 1,
+		};
+	});
+}
+
 export default function sitemap(): MetadataRoute.Sitemap {
 	return [
 		{
@@ -9,10 +22,34 @@ export default function sitemap(): MetadataRoute.Sitemap {
 			priority: 1,
 		},
 		{
+			url: "https://pyro.host/for",
+			lastModified: new Date(),
+			priority: 1,
+		},
+		{
+			url: "https://pyro.host/legal/privacy",
+			lastModified: new Date(),
+			changeFrequency: "monthly",
+			priority: 0.8,
+		},
+		{
+			url: "https://pyro.host/legal/terms",
+			lastModified: new Date(),
+			changeFrequency: "monthly",
+			priority: 0.8,
+		},
+		{
+			url: "https://pyro.host/legal/cookies",
+			lastModified: new Date(),
+			changeFrequency: "monthly",
+			priority: 0.8,
+		},
+		{
 			url: "https://pyro.host/about",
 			lastModified: new Date(),
 			changeFrequency: "monthly",
 			priority: 0.8,
 		},
+		...generateGameMap(games),
 	];
 }
