cbce4f87adcc461536e541519fc4c36012cab1ec
fero <hi@evan.graphics>
2024-05-27 05:55:18 -0700

    fix: properly load turnstile on register page

--- src/app/(auth)/register/page.tsx
+++ src/app/(auth)/register/page.tsx
@@ -9,6 +9,7 @@ import RegisterForm from "@/components/auth/RegisterForm";
 import LogoColored from "@/components/icons/LogoColored";
 
 import { getUserBySession } from "@/lib/api/user";
+import Script from 'next/script';
 
 export const metadata: Metadata = {
 	title: "Pyro - Sign Up",
@@ -21,19 +22,29 @@ export default async function Page() {
 	}
 
 	return (
-		<div className="relative m-auto flex w-full max-w-[400px] flex-col gap-4 rounded-xl border-[#ffffff15] border-[1px] bg-[#ffffff14] p-8 shadow-sm">
-			<div className="">
-				<LogoColored />
+		<>
+			<Script id="cf-turnstile-callback">
+				{`window.onloadTurnstileCallback = function () {
+          window.turnstile.render('#turnstile-widget', {
+            sitekey: '${process.env.NEXT_PUBLIC_TURNSTILE_SITE_KEY}',
+          })
+        }`}
+			</Script>
+			<Script src="https://challenges.cloudflare.com/turnstile/v0/api.js?onload=onloadTurnstileCallback" async={true} defer={true} />
+			<div className="relative m-auto flex w-full max-w-[400px] flex-col gap-4 rounded-xl border-[#ffffff15] border-[1px] bg-[#ffffff14] p-8 shadow-sm">
+				<div className="">
+					<LogoColored />
+				</div>
+				<h2 className="font-extrabold text-xl">Sign Up</h2>
+				<div className="text-sm">
+					Already have an account?{" "}
+					<Link className="text-brand" href="/login">
+						Login
+					</Link>
+				</div>
+				<OauthLogIn />
+				<RegisterForm />
 			</div>
-			<h2 className="font-extrabold text-xl">Sign Up</h2>
-			<div className="text-sm">
-				Already have an account?{" "}
-				<Link className="text-brand" href="/login">
-					Login
-				</Link>
-			</div>
-			<OauthLogIn />
-			<RegisterForm />
-		</div>
+		</>
 	);
 }
