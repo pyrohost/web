0bd6be2b821381ae589dbefe995ae08c1666fd4f
Evan Song <theevansong@gmail.com>
2024-05-30 16:31:22 -0700

    chore: make account button more distinguishable

--- src/components/ui/layout/OmnibarToolbar.tsx
+++ src/components/ui/layout/OmnibarToolbar.tsx
@@ -24,8 +24,16 @@ const HeaderLoginButton = async () => {
 					<Link
 						href="/account"
 						aria-label="Account page"
-						className="flex shrink-0 flex-row items-center justify-center rounded-full p-2 transition hover:bg-[#ffffff22]"
+						className="relative flex shrink-0 flex-row items-center justify-center rounded-full p-2 transition hover:bg-[#ffffff22]"
 					>
+						<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="absolute inset-0 flex p-4">
+							<path
+								fillRule="evenodd"
+								d="M7.5 6a4.5 4.5 0 1 1 9 0 4.5 4.5 0 0 1-9 0ZM3.751 20.105a8.25 8.25 0 0 1 16.498 0 .75.75 0 0 1-.437.695A18.683 18.683 0 0 1 12 22.5c-2.786 0-5.433-.608-7.812-1.7a.75.75 0 0 1-.437-.695Z"
+								clipRule="evenodd"
+							/>
+						</svg>
+
 						<img
 							alt=""
 							src={`https://source.boringavatars.com/marble/120/${sessionUser.id}?colors=f06d61,da825f,c4975c,a8ab7b,8cbf99`}
