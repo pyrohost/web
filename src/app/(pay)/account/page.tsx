c26814425d55a423a9d7640321990a98ab7fa227
Evan Song <theevansong@gmail.com>
2024-05-30 16:35:30 -0700

    chore: clean unused logic

--- src/app/(pay)/account/page.tsx
+++ src/app/(pay)/account/page.tsx
@@ -32,25 +32,6 @@ const Page = async ({ searchParams }: { searchParams: { [key: string]: string | 
 
 	return (
 		<div className="container mx-auto flex flex-col gap-4 py-8">
-			{searchParams?.error === "NoActiveSubscriptionsDiscordLinkedRole" && (
-				<div className="relative flex rounded-full bg-red-800/20 px-4 py-3 text-red-400" role="alert">
-					<strong className="font-bold">Failed to link Discord account! </strong>
-					<span className="ml-0.5 block sm:inline">You need to purchase something before linking your Discord.</span>
-					<Link aria-label="Close alert" className="ml-auto" href="/account">
-						<Cross1Icon className="h-6 w-6" />
-					</Link>
-				</div>
-			)}
-			{searchParams?.success === "DiscordLinkedRole" && (
-				<div className="relative flex rounded-full bg-red-800/20 px-4 py-3 text-red-400" role="alert">
-					<strong className="font-bold">Your Discord account is now linked!</strong>
-					<span className="ml-0.5 block sm:inline">Check your roles on the Pyro Discord server!</span>
-					<Link aria-label="Close alert" className="ml-auto" href="/account">
-						<Cross1Icon className="h-6 w-6" />
-					</Link>
-				</div>
-			)}
-
 			<Suspense fallback={<DashboardSkeletonSection title={"Payment Information"} />}>
 				<AccountInformation user={dbUser} />
 			</Suspense>
