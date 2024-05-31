6e43b70c5a4e7a7b2b01380f4eb74fe008ae2b45
Checksum <checksum@pyro.host>
2024-05-31 07:42:20 -0500

    chore: debugging isUserAbleToSubscribe

--- src/lib/utils/isUserAbleToSubscribe.ts
+++ src/lib/utils/isUserAbleToSubscribe.ts
@@ -2,10 +2,20 @@ import type { User } from "@prisma/client";
 import userAPI from "@/lib/api/user";
 
 export const isUserAbleToSubscribe = async (dbUser: User): Promise<boolean> => {
-	if (!dbUser.stripeCustomerId || !dbUser.pyrodactylUserId || !dbUser.emailVerified || !dbUser.firstName || !dbUser.lastName || !dbUser.phone) return false;
+	if (!dbUser.stripeCustomerId || !dbUser.pyrodactylUserId || !dbUser.emailVerified || !dbUser.firstName || !dbUser.lastName || !dbUser.phone) {
+		console.warn(
+			`User ${
+				dbUser.id
+			} is not able to subscribe\nMissing stripeCustomerId: ${!dbUser.stripeCustomerId}\nMissing pyrodactylUserId: ${!dbUser.pyrodactylUserId}\nMissing emailVerified: ${!dbUser.emailVerified}\nMissing firstName: ${!dbUser.firstName}\nMissing lastName: ${!dbUser.lastName}\nMissing phone: ${!dbUser.phone}`,
+		);
+		return false;
+	}
 
 	const address = await userAPI.getUserAddress(dbUser);
-	if (!address) return false
-	
+	if (!address) {
+		console.warn(`User ${dbUser.id} is not able to subscribe\nMissing address`);
+		return false;
+	}
+
 	return true;
 };
