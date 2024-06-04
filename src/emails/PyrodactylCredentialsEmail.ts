32a2625ce45984f45cfcfc18d8f6fb1c674cf690
Evan Song <theevansong@gmail.com>
2024-06-03 21:02:03 -0700

    chore(pyrocreds): make email more friendlyTM

--- src/emails/PyrodactylCredentialsEmail.ts
+++ src/emails/PyrodactylCredentialsEmail.ts
@@ -2,8 +2,8 @@ import type Email from "@/emails/Email";
 
 export default (email: string, password: string): Email => {
 	return {
-		subject: "Pyrodactyl Credentials",
+		subject: "Login to your new Pyro server",
 		html: undefined,
-		fallback: `Your Pyrodactyl credentials have been created. You can log in at ${process.env.PTERODACTYL_API_URL} with the following credentials:\n\nEmail: ${email}\nPassword: ${password}\n\nPlease change your password after logging in.`,
+		fallback: `Welcome to Pyro! Your Pyrodactyl credentials have been created. You can log in at ${process.env.PTERODACTYL_API_URL} with the following credentials:\n\nEmail: ${email}\nPassword: ${password}\n\nPlease change your password after logging in.`,
 	};
 };
