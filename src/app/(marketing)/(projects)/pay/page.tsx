8ef03a8e35c749bb6eaadac55d01e78b22496902
Evan Song <theevansong@gmail.com>
2024-06-09 17:10:09 -0700

    chore: finish fixing

--- src/app/(marketing)/(projects)/pay/page.tsx
+++ src/app/(marketing)/(projects)/pay/page.tsx
@@ -18,19 +18,24 @@ export const metadata: Metadata = {
 const Page = () => {
 	return (
 		<>
-			<div className="z-10 mx-auto mt-12 flex h-full w-full flex-col items-center justify-between gap-8 lg:flex-row lg:gap-0">
-				<h1 className="w-full font-extrabold text-[max(30px,min(5vw,42px))] leading-[1.2] tracking-[-0.14rem] drop-shadow-xl lg:w-1/2">
-					Pay is Pyro&apos;s dead-simple billing platform that integrates with Stripe
-				</h1>
-				<div className="flex w-full flex-col gap-8 lg:w-1/4 lg:gap-4">
-					<p className="text-[#ffffffbb] text-[20px] leading-[155%]">It&apos;s the hosting industry&apos;s easiest billing management system.</p>
-					<PyroButton variant="secondary" href="https://pay.pyro.host">
-						Try it out
-					</PyroButton>
+			<div className="relative z-10 h-full max-h-[800px] w-full overflow-clip rounded-3xl bg-brand text-white">
+				<div className="relative mx-auto flex h-full w-full container flex-col items-center px-6">
+					{" "}
+					<div className="z-10 mx-auto mt-12 flex h-full w-full flex-col items-center justify-between gap-8 lg:flex-row lg:gap-0">
+						<h1 className="w-full font-extrabold text-[max(30px,min(5vw,42px))] leading-[1.2] tracking-[-0.14rem] drop-shadow-xl lg:w-1/2">
+							Pay is Pyro&apos;s dead-simple billing platform that integrates with Stripe
+						</h1>
+						<div className="flex w-full flex-col gap-8 lg:w-1/4 lg:gap-4">
+							<p className="text-[#ffffffbb] text-[20px] leading-[155%]">It&apos;s the hosting industry&apos;s easiest billing management system.</p>
+							<PyroButton variant="secondary" href="https://pay.pyro.host">
+								Try it out
+							</PyroButton>
+						</div>
+					</div>
+					<div className="relative mt-8 min-h-[750px] w-full scale-[1.08]">
+						<Image src="/img/payhero_new.png" width={1172} height={696} className="w-full" alt="" quality={100} />
+					</div>
 				</div>
-			</div>
-			<div className="relative mt-8 min-h-[750px] w-full scale-[1.08]">
-				<Image src="/img/payhero_new.png" width={1172} height={696} className="w-full" alt="" quality={100} />
 			</div>
 		</>
 	);
